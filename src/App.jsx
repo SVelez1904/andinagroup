69164cb14c644627cc55dee900d72465e76887db
SVelez1904 <svelez1904@gmail.com>
2026-07-15 13:45:09 -0400

    solucion de error de numero de contacto

--- src/App.jsx
+++ src/App.jsx
@@ -68,7 +68,7 @@ function App() {
               <a className="btn btn--ghost btn--lg" href={FORM_URL} target="_blank" rel="noreferrer">
                 Postular mi propiedad
               </a>
-              <WhatsAppButton phoneNumber="56912345678" label="Asesorarme por WhatsApp" />
+              <WhatsAppButton phoneNumber="56983076392" label="Asesorarme por WhatsApp" />
             </div>
           </div>
           <MountainDivider tone="gold" />
